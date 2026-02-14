1bee600c4be4516db69b4e1c359652004f0107fa
Zhanna M <zhanna.myshkovskaya@gmail.com>
2026-02-14 21:22:34 +0300

    [FR-6] Added welcome page for sandboxes.

--- projects/sandbox/src/app/app.routes.ts
+++ projects/sandbox/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'button', pathMatch: 'full' },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: 'welcome', loadChildren: () => import('./showcase/welcome/welcome.module').then(m => m.WelcomeModule) },
     { path: 'button', loadChildren: () => import('./showcase/button-sandbox/button-showcase.module').then(m => m.ButtonShowcaseModule) },
     { path: 'dropdown', loadChildren: () => import('./showcase/dropdown-checkbox/dropdown-showcase.module').then(m => m.DropdownShowcaseModule) },
     { path: 'checkbox', loadChildren: () => import('./showcase/checkbox-sandbox/checkbox-showcase.module').then(m => m.CheckboxShowcaseModule) },
-    { path: '**', redirectTo: 'button' },
+    { path: '**', redirectTo: 'welcome' },
 ];
