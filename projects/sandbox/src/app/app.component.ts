988effee714140139f018b4ddcb40edd46c809ba
Zhanna M <zhanna.myshkovskaya@gmail.com>
2026-02-15 23:10:55 +0300

    [FR-13] Added responsiveness, support for mobile devices.

--- projects/sandbox/src/app/app.component.ts
+++ projects/sandbox/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +9,30 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+    public sidebarOpen = false;
+
+    private readonly router = inject(Router);
+
+    private routerSub!: Subscription;
+
+    public ngOnInit(): void {
+        this.routerSub = this.router.events
+            .pipe(filter((e) => e instanceof NavigationEnd))
+            .subscribe(() => {
+                this.sidebarOpen = false;
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this.routerSub?.unsubscribe();
+    }
+
+    public toggleSidebar(): void {
+        this.sidebarOpen = !this.sidebarOpen;
+    }
+
+    public closeSidebar(): void {
+        this.sidebarOpen = false;
+    }
+}
