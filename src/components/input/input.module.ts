683fe50e4a7abf337c3a0718e983ffe5de3fa320
Zhanna M <zhanna.myshkovskaya@gmail.com>
2026-02-19 22:49:14 +0300

    [FR-16] Updated input field to include clear field icon.

--- src/components/input/input.module.ts
+++ src/components/input/input.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { InputComponent } from './input.component';
+import { IconModule } from '../icon/icon.module';
 
 @NgModule({
     declarations: [InputComponent],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, IconModule],
     exports: [InputComponent],
 })
 export class InputModule {}
